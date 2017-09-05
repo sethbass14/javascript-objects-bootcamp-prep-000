d581acaf70f34ff95ad93932a3f1092378a3e9c1
Seth Barden <sethbarden@gmail.com>
2017-09-05 22:24:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,14 @@
-var playlist = {Micheal_Jackson: "Beat It",
-                Steely_Dan: "Peg",
+var playlist = {Micheal Jackson: "Beat It",
+                Steely Dan: "Peg",
                 EWF: "September"
+}
+
+function updatePlaylist(list, artist, song) {
+  list.artist = 'song';
+  return list
+}
+
+function removeFromPlaylist(list, artist){
+  delete list.artist
+  return list
 }
