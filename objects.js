cbdd7d359d754d37a1bae6446d0cf5892a78ff57
Seth Barden <sethbarden@gmail.com>
2017-09-05 22:36:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
-  return playlist
+  return Object.assign({}, playlist, delete playlist.artistName)
 }
